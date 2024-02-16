01ce3221c6761c7c91332a339729e56574d912c1
Krishna Murali Andiboyina <kmandiboyina@gmail.com>
2024-02-16 10:56:22 +0530

    Bookings.cs

---
+++ BusinessLayer/Bookings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Bookings
+    {
+        public int? EventBookingId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? MobileNumber { get; set; }
+        public int? VenueId { get; set; }
+        public string? VenueName { get; set; }
+        public DateTime? EventDateTime { get; set; }
+        public int? BookedHours { get; set; }
+
+    }
+}
